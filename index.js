32d28c3f0cea50467f5cf3a64b6e71097af0b5d5
pavel <pavel@setupad.com>
2022-08-23 19:33:17 +0200

    add completed && active

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 let sortByName = false;
+let sortByDate = false;
+let taskStatus = "active"; //status can be: active and completed
 
 //function to draw a new task
 const addTask = () => {
@@ -14,6 +16,7 @@ const addTask = () => {
   const newTask = {
     id: new Date().getTime(),
     task: inputValue,
+    status: "active", //status can be: active and completed
   };
 
   const lsArr = JSON.parse(window.localStorage.getItem("tasks"));
@@ -35,6 +38,20 @@ document.getElementById("taskInput").addEventListener("keypress", (e) => {
   }
 });
 
+// function to read date
+const formatDate = (date) => {
+  const addZero = (num) => (num < 10 ? "0" + num : num);
+
+  const h = addZero(new Date(date).getHours());
+  const min = addZero(new Date(date).getMinutes());
+  const s = addZero(new Date(date).getSeconds());
+  const d = addZero(new Date(date).getDate());
+  const mon = addZero(new Date(date).getMonth() + 1);
+  const y = new Date(date).getFullYear();
+
+  return `${h}:${min}:${s} \xa0 ${d}-${mon}-${y}`;
+};
+
 const drawTaskList = () => {
   const tasksList = document.getElementById("tasksList");
   tasksList.innerHTML = null;
@@ -42,31 +59,47 @@ const drawTaskList = () => {
   //get data from localStorage
   const lsArr = JSON.parse(window.localStorage.getItem("tasks"));
   let arr = lsArr ? lsArr : [];
-
+  console.log(lsArr);
   if (sortByName) {
-    console.log("sort triggered");
     arr.sort((a, b) => (a.task > b.task ? 1 : b.task > a.task ? -1 : 0));
   }
-  console.log("sortByName", sortByName);
-  console.log("tasks", arr);
+
+  if (sortByDate) {
+    arr.sort((a, b) => (a.id > b.id ? 1 : b.id > a.id ? -1 : 0));
+  }
+
+  // filtering active
+  if (taskStatus === "active") {
+    arr = arr.filter(({ status }) => status === "active");
+  }
+
+  // filtering completed
+  if (taskStatus === "completed") {
+    arr = arr.filter(({ status }) => status === "completed");
+  }
+
   arr.forEach((singleTask, ind) => {
     //creating elements
     const myLi = document.createElement("li");
     const myInput = document.createElement("input");
     const myLabel = document.createElement("label");
+    const myDate = document.createElement("div");
     //btn group
     const btnGroup = document.createElement("div");
     const deleteBtn = document.createElement("button");
     const editBtn = document.createElement("button");
+    const completeBtn = document.createElement("button");
 
     //adding styles
-    myLi.className = "container list-group-item";
+    myLi.className = "row d-flex justify-content-between list-group-item";
     myInput.className = "form-check-input me-1 col-1";
-    myLabel.className = "form-check-label col-9";
+    myLabel.className = "form-check-label col-4";
+    myDate.className = "col-5";
 
     btnGroup.className = "btn-group col-2";
     deleteBtn.className = "btn btn-outline-danger btn-sm ";
     editBtn.className = "btn btn-outline-primary btn-sm";
+    completeBtn.className = "btn btn-outline-success btn-sm";
 
     //adding other attributes
     //input
@@ -76,6 +109,9 @@ const drawTaskList = () => {
     //label
     myLabel.setAttribute("for", ind);
     myLabel.textContent = singleTask.task;
+
+    //date
+    myDate.textContent = formatDate(singleTask.id);
 
     //btn group
     btnGroup.setAttribute("role", "group");
@@ -88,16 +124,34 @@ const drawTaskList = () => {
     editBtn.setAttribute("type", "button");
     editBtn.textContent = "Edit";
 
+    // complete on click
+    completeBtn.setAttribute("type", "button");
+    completeBtn.textContent = "Done";
+
     //append childs
     //handle btn group append
-    btnGroup.append(editBtn, deleteBtn);
-    myLi.append(myInput, myLabel, btnGroup);
+    switch (taskStatus) {
+      case "active":
+        btnGroup.append(editBtn, completeBtn, deleteBtn);
+        break;
+      case "completed":
+        btnGroup.append(deleteBtn);
+        break;
+      default:
+        btnGroup.append(editBtn, completeBtn, deleteBtn);
+        break;
+    }
+
+    myLi.append(myInput, myLabel, myDate, btnGroup);
     tasksList.append(myLi);
 
     //task events
     deleteBtn.addEventListener("click", () => {
-      arr = arr.filter((filterTask) => filterTask.id !== singleTask.id);
-      window.localStorage.setItem("tasks", JSON.stringify(arr));
+      console.log(lsArr);
+      const filteredArr = lsArr.filter(
+        (filterTask) => filterTask.id !== singleTask.id
+      );
+      window.localStorage.setItem("tasks", JSON.stringify(filteredArr));
       drawTaskList();
     });
 
@@ -110,17 +164,64 @@ const drawTaskList = () => {
           task: updatedTask,
         };
 
-        arr.splice(ind, 1, newTask);
+        const updatedArr = lsArr.map((task) =>
+          task.id === singleTask.id ? newTask : task
+        );
+
+        window.localStorage.setItem("tasks", JSON.stringify(updatedArr));
+        drawTaskList();
+      }
+    });
+
+    completeBtn.addEventListener("click", () => {
+      if ((singleTask.status = "active")) {
+        const newTask = {
+          ...singleTask,
+          status: "completed",
+        };
+
+        const updatedArr = lsArr.map((task) =>
+          task.id === singleTask.id ? newTask : task
+        );
+
+        window.localStorage.setItem("tasks", JSON.stringify(updatedArr));
         drawTaskList();
       }
     });
   });
 };
 
+// sorting by name
 document.getElementById("sortByName").addEventListener("click", function () {
   sortByName = !sortByName;
-
   sortByName ? this.classList.add("active") : this.classList.remove("active");
+  document.getElementById("sortByDate").classList.remove("active");
+  sortByDate = false;
+  drawTaskList();
+});
+
+// sorting by date
+document.getElementById("sortByDate").addEventListener("click", function () {
+  sortByDate = !sortByDate;
+  sortByDate ? this.classList.add("active") : this.classList.remove("active");
+  document.getElementById("sortByName").classList.remove("active");
+  sortByName = false;
+  drawTaskList();
+});
+
+// filtering active
+document.getElementById("activeTask").addEventListener("click", function () {
+  taskStatus = "active";
+  document.getElementById("completedTask").classList.remove("active-nav");
+  this.classList.add("active-nav");
+  drawTaskList();
+});
+
+// filtering completed
+document.getElementById("completedTask").addEventListener("click", function () {
+  taskStatus = "completed";
+  document.getElementById("activeTask").classList.remove("active-nav");
+  this.classList.add("active-nav");
   drawTaskList();
 });
 
